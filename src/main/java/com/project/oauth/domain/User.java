f3bf5315c986586ed57cf69dce24efa34f7bb60b
ho_ong8 <honghj8959@gmail.com>
2023-03-06 20:54:55 +0900

    add google oauth2 login function

---
+++ src/main/java/com/project/oauth/domain/User.java
@@ -0,0 +1,49 @@
+package com.project.oauth.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Role role;
+
+    @Builder
+    public User(Long id, String name, String email, String password, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User update(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+}
